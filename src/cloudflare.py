59f2e406463f89c96dda03ff161629706778e4e7
manhd89 <150607244+manhd89@users.noreply.github.com>
2024-01-29 13:28:29 +0700

    Update cloudflare.py

--- src/cloudflare.py
+++ src/cloudflare.py
@@ -1,92 +1,109 @@
-import json
+import time
 from src import (
     error,
     session,
+    silent_error,
     BASE_URL,
     MAX_LIST_SIZE
 )
 
+MAX_RETRY = 3
+
 def get_current_lists():
-    response = session.get(
-        f"{BASE_URL}/lists"
-    )
-    if response.status_code == 200:
-        return response.json()
-    else:
-        error("Failed to get current lists")
+    for _ in range(MAX_RETRY):
+        try:
+            response = session.get(f"{BASE_URL}/lists")
+            if response.status_code == 200:
+                return response.json()
+        except Exception as e:
+            silent_error(f"Failed to get current lists. Error: {e}")
+        time.sleep(1)
+    error("Failed to get current lists after retries")
 
 def get_current_policies():
-    response = session.get(
-        f"{BASE_URL}/rules"
-    )
-    if response.status_code == 200:
-        return response.json()
-    else:
-        error("Failed to get current policies")
+    for _ in range(MAX_RETRY):
+        try:
+            response = session.get(f"{BASE_URL}/rules")
+            if response.status_code == 200:
+                return response.json()
+        except Exception as e:
+            silent_error(f"Failed to get current policies. Error: {e}")
+        time.sleep(1)  
+    error("Failed to get current policies after retries")
 
 def get_list_items(list_id):
-    response = session.get(
-        f"{BASE_URL}/lists/{list_id}/items?limit={MAX_LIST_SIZE}"
-    )
-    if response.status_code == 200:
-        return response.json()
-    else:
-        error(f"Failed to get list items")
+    for _ in range(MAX_RETRY):
+        try:
+            response = session.get(f"{BASE_URL}/lists/{list_id}/items?limit={MAX_LIST_SIZE}")
+            if response.status_code == 200:
+                return response.json()
+        except Exception as e:
+            silent_error(f"Failed to get list items. Error: {e}")
+        time.sleep(1)  
+    error("Failed to get list items after retries")
 
 def patch_list(list_id, payload):
-    response = session.patch(
-        f"{BASE_URL}/lists/{list_id}",
-        json=payload
-    )
-    if response.status_code == 200:
-        return response.json()
-    else:
-        error(f"Failed to patch list")
+    for _ in range(MAX_RETRY):
+        try:
+            response = session.patch(f"{BASE_URL}/lists/{list_id}",json=payload)
+            if response.status_code == 200:
+                return response.json()
+        except Exception as e:
+            silent_error(f"Failed to get patch list. Error: {e}")
+        time.sleep(1)  
+    error("Failed to get patch list after retries")
 
 def create_list(payload):
-    response = session.post(
-        f"{BASE_URL}/lists",
-        json=payload
-    )
-    if response.status_code == 200:
-        return response.json()
-    else:
-        error("Failed to create list")
+    for _ in range(MAX_RETRY):
+        try:
+            response = session.post(f"{BASE_URL}/lists",json=payload)
+            if response.status_code == 200:
+                return response.json()
+        except Exception as e:
+            silent_error(f"Failed to get create list. Error: {e}")
+        time.sleep(1)  
+    error("Failed to get create list after retries")
 
 def create_policy(json_data):
-    response = session.post(
-        f"{BASE_URL}/rules",
-        json=json_data
-    )
-    if response.status_code == 200:
-        return response.json()
-    else:
-        error("Failed to create policy")
+    for _ in range(MAX_RETRY):
+        try:
+            response = session.post(f"{BASE_URL}/rules",json=json_data)
+            if response.status_code == 200:
+                return response.json()
+        except Exception as e:
+            silent_error(f"Failed to get create policy. Error: {e}")
+        time.sleep(1)  
+    error("Failed to get create policy after retries")
 
 def update_policy(policy_id, json_data):
-    response = session.put(
-        f"{BASE_URL}/rules/{policy_id}",
-        json=json_data
-    )
-    if response.status_code == 200:
-        return response.json()
-    else:
-        error(f"Failed to update policy")
+    for _ in range(MAX_RETRY):
+        try:
+            response = session.put(f"{BASE_URL}/rules/{policy_id}",json=json_data)
+            if response.status_code == 200:
+                return response.json()
+        except Exception as e:
+            silent_error(f"Failed to get update policy. Error: {e}")
+        time.sleep(1)  
+    error("Failed to get update policy after retries")
 
 def delete_list(list_id):
-    response = session.delete(
-        f"{BASE_URL}/lists/{list_id}"
-    )
-    if response.status_code == 200:
-        return response.json()
-    else:
-        error(f"Failed to delete list")
+    for _ in range(MAX_RETRY):
+        try:
+            response = session.delete(f"{BASE_URL}/lists/{list_id}")
+            if response.status_code == 200:
+                return response.json()
+        except Exception as e:
+            silent_error(f"Failed to get delete list. Error: {e}")
+        time.sleep(1)  
+    error("Failed to get delete list after retries")
 
 def delete_policy(policy_id):
-    response = session.delete(
-        f"{BASE_URL}/rules/{policy_id}"
-    )
-    if response.status_code == 200:
-        return response.json()
-    else:
-        error(f"Failed to delete policy")
+    for _ in range(MAX_RETRY):
+        try:
+            response = session.delete(f"{BASE_URL}/rules/{policy_id}")
+            if response.status_code == 200:
+                return response.json()
+        except Exception as e:
+            silent_error(f"Failed to get delete policy. Error: {e}")
+        time.sleep(1)  
+    error("Failed to get delete policy after retries")
