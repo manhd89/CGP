a4193603d3302deefaae570195f645cd83d4be78
manhd89 <150607244+manhd89@users.noreply.github.com>
2024-06-24 20:26:51 +0700

    Test

--- src/cloudflare.py
+++ src/cloudflare.py
@@ -1,7 +1,7 @@
 import json
 from http.client import HTTPException
 from . import (
-    info, headers, MAX_LIST_SIZE, rate_limited_request,perform_request,
+    info, rate_limited_request, perform_request,
     retry, stop_never, wait_random_exponential, retry_if_exception_type
 )
 
@@ -21,52 +21,52 @@ retry_config = {
 
 @retry(**retry_config)
 def get_current_lists():
-    status, data = perform_request("GET", "/lists", headers)
+    status, data = perform_request("GET", "/lists")
     return data
 
 @retry(**retry_config)
 def get_current_policies():
-    status, data = perform_request("GET", "/rules", headers)
+    status, data = perform_request("GET", "/rules")
     return data
 
 @retry(**retry_config)
 def get_list_items(list_id):
-    status, data = perform_request("GET", f"/lists/{list_id}/items?limit={MAX_LIST_SIZE}", headers)
+    status, data = perform_request("GET", f"/lists/{list_id}/items?limit=1000")
     return data
 
 @retry(**retry_config)
 @rate_limited_request
 def patch_list(list_id, payload):
     body = json.dumps(payload)
-    status, data = perform_request("PATCH", f"/lists/{list_id}", headers, body)
+    status, data = perform_request("PATCH", f"/lists/{list_id}", body)
     return data
 
 @retry(**retry_config)
 @rate_limited_request
 def create_list(payload):
     body = json.dumps(payload)
-    status, data = perform_request("POST", "/lists", headers, body)
+    status, data = perform_request("POST", "/lists", body)
     return data
 
 @retry(**retry_config)
 def create_policy(json_data):
     body = json.dumps(json_data)
-    status, data = perform_request("POST", "/rules", headers, body)
+    status, data = perform_request("POST", "/rules", body)
     return data
 
 @retry(**retry_config)
 def update_policy(policy_id, json_data):
     body = json.dumps(json_data)
-    status, data = perform_request("PUT", f"/rules/{policy_id}", headers, body)
+    status, data = perform_request("PUT", f"/rules/{policy_id}", body)
     return data
 
 @retry(**retry_config)
 @rate_limited_request
 def delete_list(list_id):
-    status, data = perform_request("DELETE", f"/lists/{list_id}", headers)
+    status, data = perform_request("DELETE", f"/lists/{list_id}")
     return data
 
 @retry(**retry_config)
 def delete_policy(policy_id):
-    status, data = perform_request("DELETE", f"/rules/{policy_id}", headers)
+    status, data = perform_request("DELETE", f"/rules/{policy_id}")
     return data
