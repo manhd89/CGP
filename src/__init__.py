e6dac65907f06edaee3bf06b712020c2b2b1c890
manhd89 <150607244+manhd89@users.noreply.github.com>
2024-06-27 09:10:00 +0700

    Update __init__.py

--- src/__init__.py
+++ src/__init__.py
@@ -9,7 +9,7 @@ from functools import wraps
 # Constants
 MAX_LISTS = 300
 MAX_LIST_SIZE = 1000
-RATE_LIMIT_INTERVAL = 1.0
+RATE_LIMIT_INTERVAL = 0.3
 PREFIX = "AdBlock-DNS-Filters"
 
 # Read .env variables 
@@ -47,42 +47,6 @@ ip_pattern = re.compile(
     r"^\d{1,3}\.\d{1,3}\.\d{1,3}\.\d{1,3}$"
 )
 
-BASE_URL = f"https://api.cloudflare.com/client/v4/accounts/{CF_IDENTIFIER}/gateway"
-
-# Retry decorator
-def retry(stop=None, wait=None, retry=None, after=None, before_sleep=None):
-    def decorator(func):
-        @wraps(func)
-        def wrapper(*args, **kwargs):
-            attempt_number = 0
-            while True:
-                try:
-                    attempt_number += 1
-                    return func(*args, **kwargs)
-                except Exception as e:
-                    if retry and not retry(e):
-                        raise
-                    if after:
-                        after({'attempt_number': attempt_number, 'outcome': e})
-                    if stop and stop(attempt_number):
-                        raise
-                    if before_sleep:
-                        before_sleep({'attempt_number': attempt_number})
-                    wait_time = wait(attempt_number) if wait else 1
-                    time.sleep(wait_time)
-        return wrapper
-    return decorator
-
-# Retry utility functions
-def stop_never(attempt_number):
-    return False
-
-def wait_random_exponential(attempt_number, multiplier=1, max_wait=10):
-    return min(multiplier * (2 ** random.uniform(0, attempt_number - 1)), max_wait)
-
-def retry_if_exception_type(exceptions):
-    return lambda e: isinstance(e, exceptions)
-
 # Logging functions
 def error(message):
     logger.error(message)
@@ -93,27 +57,3 @@ def silent_error(message):
 
 def info(message):
     logger.info(message)
-
-# Rate limiter
-class RateLimiter:
-    def __init__(self, interval):
-        self.interval = interval
-        self.timestamp = time.time()
-
-    def wait_for_next_request(self):
-        now = time.time()
-        elapsed = now - self.timestamp
-        sleep_time = max(0, self.interval - elapsed)
-        if (sleep_time > 0):
-            time.sleep(sleep_time)
-        self.timestamp = time.time()
-
-rate_limiter = RateLimiter(1.0)
-
-# Function to limit requests
-def rate_limited_request(func):
-    @wraps(func)
-    def wrapper(*args, **kwargs):
-        rate_limiter.wait_for_next_request()
-        return func(*args, **kwargs)
-    return wrapper
