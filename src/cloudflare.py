1e386e582e2316fb61d186bfe4c6f1f5ae221579
manhd89 <150607244+manhd89@users.noreply.github.com>
2024-06-27 08:55:52 +0700

    Update cloudflare.py

--- src/cloudflare.py
+++ src/cloudflare.py
@@ -1,5 +1,5 @@
 from src.requests import session, HTTPError, RequestException
-from src import info,BASE_URL, MAX_LIST_SIZE, rate_limited_request, retry, stop_never, wait_random_exponential, retry_if_exception_type
+from src import info, MAX_LIST_SIZE, rate_limited_request, retry, stop_never, wait_random_exponential, retry_if_exception_type
 import json
 
 retry_config = {
@@ -18,48 +18,48 @@ retry_config = {
 
 @retry(**retry_config)
 def get_current_lists():
-    response = session.get(f"{BASE_URL}/lists")
+    response = session.get(f"/lists")
     return json.loads(response)
 
 @retry(**retry_config)
 def get_current_policies():
-    response = session.get(f"{BASE_URL}/rules")
+    response = session.get(f"/rules")
     return json.loads(response)
 
 @retry(**retry_config)
 def get_list_items(list_id):
-    response = session.get(f"{BASE_URL}/lists/{list_id}/items?limit={MAX_LIST_SIZE}")
+    response = session.get(f"/lists/{list_id}/items?limit={MAX_LIST_SIZE}")
     return json.loads(response)
 
 @retry(**retry_config)
 @rate_limited_request
 def patch_list(list_id, payload):
-    response = session.patch(f"{BASE_URL}/lists/{list_id}", json=payload)
+    response = session.patch(f"/lists/{list_id}", json=payload)
     return json.loads(response)
 
 @retry(**retry_config)
 @rate_limited_request
 def create_list(payload):
-    response = session.post(f"{BASE_URL}/lists", json=payload)
+    response = session.post(f"/lists", json=payload)
     return json.loads(response)
 
 @retry(**retry_config)
 def create_policy(json_data):
-    response = session.post(f"{BASE_URL}/rules", json=json_data)
+    response = session.post(f"/rules", json=json_data)
     return json.loads(response)
 
 @retry(**retry_config)
 def update_policy(policy_id, json_data):
-    response = session.patch(f"{BASE_URL}/rules/{policy_id}", json=json_data)
+    response = session.patch(f"/rules/{policy_id}", json=json_data)
     return json.loads(response)
 
 @retry(**retry_config)
 @rate_limited_request
 def delete_list(list_id):
-    response = session.delete(f"{BASE_URL}/lists/{list_id}")
+    response = session.delete(f"/lists/{list_id}")
     return json.loads(response)
 
 @retry(**retry_config)
 def delete_policy(policy_id):
-    response = session.delete(f"{BASE_URL}/rules/{policy_id}")
+    response = session.delete(f"/rules/{policy_id}")
     return json.loads(response)
